d6ea0d7d33ed910c93a2efcdad0fcda13f863ba0
maryna.babkina <babckina@gmail.com>
2020-11-15 18:58:53 +0200

    variables renaming

--- hw2/index.js
+++ hw2/index.js
@@ -1,15 +1,15 @@
-let N = prompt('Please enter integer, which marks the start point of caluclations');
+let numberStartFrom = prompt('Please enter integer, which marks the start point of caluclations');
 
-let M = prompt('Please enter integer, which marks the end point of caluclations');
+let numberEndWith = prompt('Please enter integer, which marks the end point of caluclations');
 
 const skipEven = confirm('Do you want to skip even numbers?');
 
-N = parseInt(N, 10);
-M = parseInt(M, 10);
+N = parseInt(numberStartFrom, 10);
+M = parseInt(numberEndWith, 10);
 
 let result = null;
 
-for (let i = N; !(isNaN(N)) && !(isNaN(N)) && i <= M; i++) {
+for (let i = numberStartFrom; !(isNaN(numberStartFrom)) && !(isNaN(numberEndWith)) && i <= numberEndWith; i++) {
     if (skipEven) {
         for (let j = i;
             (j % 2) !== 0; j++) {
