b1472a1b7ddee2166d623d5d11070e01cf7af8de
maryna.babkina <babckina@gmail.com>
2020-11-21 20:18:10 +0200

    hw2 refactoring after feedback

--- hw2/index.js
+++ hw2/index.js
@@ -1,23 +1,20 @@
-let numberStartFrom = prompt('Please enter integer, which marks the start point of caluclations');
+let numberStartFrom = +prompt('Please enter integer, which marks the start point of calculations');
+while (!Number.isInteger(numberStartFrom)) numberStartFrom = +prompt('Please enter INTEGER nubmers only');
 
-let numberEndWith = prompt('Please enter integer, which marks the end point of caluclations');
+let numberEndWith = +prompt('Please enter integer, which marks the end point of caluclations');
+while (!Number.isInteger(numberEndWith)) numberEndWith = +prompt('Please enter INTEGER nubmers only');
+while (numberEndWith <= numberStartFrom) numberEndWith = +prompt('Please enter number BIGGER than start number');
 
 const skipEven = confirm('Do you want to skip even numbers?');
 
-numberStartFrom = parseInt(numberStartFrom, 10);
-numberEndWith = parseInt(numberEndWith, 10);
-
 let result = null;
 
-for (let i = numberStartFrom; !(isNaN(numberStartFrom)) && !(isNaN(numberEndWith)) && i <= numberEndWith; i++) {
-    if (skipEven) {
-        for (let j = i;
-            (j % 2) !== 0; j++) {
-            result += j;
-        }
-    } else {
-        result += i;
+for (let i = numberStartFrom; i <= numberEndWith; i++) {
+    if (skipEven && i % 2 === 0) {
+        continue
     }
+    result += i;
 }
 
-document.writeln(`The sum is: ${result ? result : 'Please check your inputs'}`);
+
+document.writeln(`The sum is: ${result}`);
