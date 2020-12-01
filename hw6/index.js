a066e8fb5016081d15845f968a4d309bd64ccaee
maryna.babkina <babckina@gmail.com>
2020-12-02 00:03:01 +0200

    hw6 editing replace function

--- hw6/index.js
+++ hw6/index.js
@@ -27,17 +27,15 @@ const students = [{
 
 /* Function 1 - Subjects list for a student*/
 
-const subjectsList = function getSubjects(studentIndex) {
-    return Object.keys(students[studentIndex].subjects).map((s) => {
-        return (s[0].toUpperCase() + s.slice(1).toLowerCase());
-    })
-};
+const getSubjects = (studentIndex) => Object.keys(students[studentIndex].subjects).map((s) => {
+    return (s[0].toUpperCase() + s.slice(1).toLowerCase().replace('_', ' '));
+});
 
-console.log(subjectsList(1));
+console.log(getSubjects(0));
 
 /* Function 2 - Avarage mark for a student */
 
-const average = function getAverage(array) {
+const getAverage = (array) => {
     const integerArray = array.filter((number) => Number.isInteger(+number));
 
     const sum = integerArray.reduce((summedArray, number) => {
@@ -47,29 +45,27 @@ const average = function getAverage(array) {
     return +(sum / integerArray.length).toFixed(2);
 }
 
-const averageMark = function getAverageMark(studentIndex) {
-    return average(Object.values(students[studentIndex].subjects).join().split(','))
-}
+const getAverageMark = (studentIndex) => getAverage(Object.values(students[studentIndex].subjects).join().split(','))
 
-console.log(averageMark(0));
+console.log(getAverageMark(0));
 
 /* Function 3 - Student info */
 
-const studentInfo = function getStudentInfo(studentIndex) {
+const getStudentInfo = (studentIndex) => {
     const infoObject = {
         course: students[studentIndex].course,
         name: students[studentIndex].name,
-        avrageMark: averageMark(studentIndex),
+        avrageMark: getAverageMark(studentIndex),
     };
 
     return infoObject;
 }
 
-console.log(studentInfo(2));
+console.log(getStudentInfo(2));
 
 /* Function 4 - Get name in alpabetical order */
 
-const studentsNames = function getStudentsNames(array) {
+const getStudentsNames = (array) => {
     const names = [];
     array.forEach(student => {
         names.push(student.name)
@@ -78,13 +74,13 @@ const studentsNames = function getStudentsNames(array) {
     return names.sort()
 }
 
-console.log(studentsNames(students));
+console.log(getStudentsNames(students));
 
 /* Function 5  - Best students */
-const bestStudent = function getBestStudent(array) {
+const getBestStudent = (array) => {
     const averageMarks = [];
     array.forEach(student => {
-        averageMarks.push(averageMark(array.indexOf(student)))
+        averageMarks.push(getAverageMark(array.indexOf(student)))
     });
 
     const highestMark = Math.max(...averageMarks);
@@ -93,42 +89,42 @@ const bestStudent = function getBestStudent(array) {
     return array[index].name;
 }
 
-console.log(bestStudent(students));
+console.log(getBestStudent(students));
 
 
 /* Function 6 - String to object */
 
-const noDuplicates = function deleteDuplicateLetter(string) { /* delete duplicated letters to use as keys */
+const deleteDuplicateLetter = (string) => { /* delete duplicated letters to use as keys */
     let counter;
     let noDuplcatesString = string;
     for (i = 0; i < noDuplcatesString.length; i++) {
-        counter = letterCount(string, string.charAt(i));
+        counter = countLetter(string, string.charAt(i));
 
         if (counter > 1) noDuplcatesString.slice(i, 1);
     }
     return noDuplcatesString;
 }
 
-const letterCount = function countLetter(word, letter) { /* count letters to use as values */
+const countLetter = (word, letter) => { /* count letters to use as values */
     let counter = 0;
     for (let i = 0; i < word.length; i++) {
         if (word.toLowerCase().charAt(i) === letter.toLowerCase()) counter++;
     }
 
-    return counter
+    return counter;
 }
 
-const stringToObject = function convertStringToObject(string) {
+const convertStringToObject = (string) => {
     const resultObject = {};
-    let keysArray = [...noDuplicates(string)];
+    let keysArray = [...deleteDuplicateLetter(string)];
     const valuesArray = [];
 
     keysArray.forEach(key => {
-        valuesArray.push(letterCount(string, key))
+        valuesArray.push(countLetter(string, key))
     });
 
     keysArray.forEach((key, i) => resultObject[key] = valuesArray[i]);
     return resultObject
 }
 
-console.log(stringToObject('test'));
+console.log(convertStringToObject('test'));
