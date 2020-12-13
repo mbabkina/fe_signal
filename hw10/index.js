ae19387cc0eee3959e79f6a7001c8749511f23c4
maryna.babkina <babckina@gmail.com>
2020-12-13 23:04:03 +0200

    hw10 initial commit

--- hw10/index.js
+++ hw10/index.js
@@ -1,70 +1,23 @@
 let keysSounds = {
-    80: 'piano',
-    71: 'guitar',
-    68: 'drum',
-    67: 'conga_tap',
-    66: 'cymbal',
-    76: 'shake',
-    72: 'synt_down',
-    77: 'synt_up',
+    p: 'piano',
+    g: 'guitar',
+    d: 'drum',
+    c: 'conga_tap',
+    b: 'cymbal',
+    l: 'shake',
+    h: 'synt_down',
+    m: 'synt_up',
 }
 
-document.querySelector('.button-wrapper').addEventListener('click', function() {
+document.querySelector('.button-wrapper').addEventListener('click', function(el) {
+    if (el.target.closest('button')) {
+        let soundId = keysSounds[el.target.id]
+        if (soundId) document.getElementById(soundId).play()
 
-})
-
-/* document.querySelector('#sound-P').addEventListener('click', function() {
-    let audio = new Audio();
-    audio.src = 'sounds/piano.mp3';
-    audio.autoplay = true;
-})
-
-document.querySelector('#sound-P').addEventListener('keydown', function() {
-    if (key.code == 80) {
-        let audio = new Audio();
-        audio.src = 'sounds/piano.mp3';
-        audio.autoplay = true;
     }
 })
 
-document.querySelector('#sound-G').addEventListener('click', function() {
-    let audio = new Audio();
-    audio.src = 'sounds/guitar.mp3';
-    audio.autoplay = true;
-})
-
-document.querySelector('#sound-D').addEventListener('click', function() {
-    let audio = new Audio();
-    audio.src = 'sounds/c.mp3';
-    audio.autoplay = true;
-})
-
-document.querySelector('#sound-C').addEventListener('click', function() {
-    let audio = new Audio();
-    audio.src = 'sounds/conga_tap.mp3';
-    audio.autoplay = true;
-})
-
-document.querySelector('#sound-B').addEventListener('click', function() {
-    let audio = new Audio();
-    audio.src = 'sounds/cymbal.mp3';
-    audio.autoplay = true;
-})
-
-document.querySelector('#sound-L').addEventListener('click', function() {
-    let audio = new Audio();
-    audio.src = 'sounds/shake.mp3';
-    audio.autoplay = true;
-})
-
-document.querySelector('#sound-H').addEventListener('click', function() {
-    let audio = new Audio();
-    audio.src = 'sounds/synt_down.mp3';
-    audio.autoplay = true;
-})
-
-document.querySelector('#sound-M').addEventListener('click', function() {
-    let audio = new Audio();
-    audio.src = 'sounds/synt_up.mp3';
-    audio.autoplay = true;
-}) */
+document.onkeydown = function(el) {
+    let soundId = keysSounds[el.key];
+    if (soundId) document.getElementById(soundId).play()
+}
